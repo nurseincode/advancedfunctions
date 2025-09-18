686e8992ad5a82e7aeea6a1be4815004212526c4
https://github.com/nurseincode <nurseincode@gmail.com>
2025-09-18 13:07:55 +1000

    cb

--- callbacks.py
+++ callbacks.py
@@ -1,9 +1,20 @@
-def greet(name):
+import time
+
+def greet(name, cb):
     print(f'Hello, {name}!')
+    # time.sleep(2) # Program pauses here
+    cb() # Runs after the pause
 
 def say_bye():
     print('Bye!')
 
+def shout():
+    print('GOODBYE!!')
+
+
 # Main
-greet('Faith')
-say_bye()
+greet('Faith', say_bye) # say_bye function passed as a variable, cb
+greet('Tim', shout)
+print('Continuing main ...')
+
+
